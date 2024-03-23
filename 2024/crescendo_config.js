d1fa3b20380ddb4995cfe4dc9782443d95960b2a
Andy Le <tandyqle@gmail.com>
2024-03-23 09:33:00 -0400

    Update crescendo_config.js

--- 2024/crescendo_config.js
+++ 2024/crescendo_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2024mdsev",
+      "defaultValue": "2024mdowi",
       "required": "true"
     },
     { "name": "Match Level",
